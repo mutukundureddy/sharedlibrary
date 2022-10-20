a35bad29ef2ae2ba5f1f1f0177bdfe6504835e03
mutukundureddy <115526332+mutukundureddy@users.noreply.github.com>
2022-10-20 12:15:13 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum(a,c)
+{
+   echo" the sum of ${a} and ${c} is ${a+c}"
+}
+def sub(a,c)
+{
+   echo" the subtraction of ${a} and ${c} is ${a-c}"
+}
+def mul(a,c)
+{
+   echo" the multiplication of ${a} and ${c} is ${a*c}"
+}
+def div(a,c)
+{
+   echo" the division of ${a} and ${c} is ${a/c}"
+}
