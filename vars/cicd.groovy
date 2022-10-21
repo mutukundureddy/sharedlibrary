065a3c4b2cb64d65415c9f29ee934f0bd4581596
mutukundureddy <115526332+mutukundureddy@users.noreply.github.com>
2022-10-21 13:02:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,7 +7,13 @@ def newmaven()
 {
   sh'mvn package'
 }
-def newdeploy(jobname,ip,context)
+
+def newdeploy (jobname,ip,context)
 {
   sh" scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+ 
+def newselenium (jobname)
+{
+  sh" java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
 }
