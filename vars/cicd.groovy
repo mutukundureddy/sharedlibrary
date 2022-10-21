2692c900d17abb647d0b80d5ee854c0707e3ca6e
mutukundureddy <115526332+mutukundureddy@users.noreply.github.com>
2022-10-21 12:19:35 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,7 @@ def newmaven()
 {
   sh'mvn package'
 }
+def newdeploy(jobname,ip,context)
+{
+  sh'/home/ubuntu/.jenkins/workspace/declarative pipeline with shared libraray/webapp/target/webapp.war ubuntu@172.31.3.89:/var/lib/tomcat9/webapps/testap.war'
+}
